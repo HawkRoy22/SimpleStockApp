01f29c304828c8219164e5bfb27722c2c1d4f8c4
Aneek <44777979+HawkRoy22@users.noreply.github.com>
2021-08-24 12:57:20 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pandas as pd
 st.write("""
 # Simple Stock Price App
 
+Shown are the stock  **closing price** and **volume** of Google!
+
 """)
 
 # Defining Ticker Symbol
@@ -13,8 +15,14 @@ tickerSymbol = 'GOOGL'
 # Get Data Down on This Ticker
 tickerData = yf.Ticker(tickerSymbol)
 
-# Get the historical prices for this ticker
+# Get the historical prices for this ticket
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
